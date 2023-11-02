f7166c136386812d7a8cfdff81f60af584cea866
Nabil Hadi <napilfadl@gmail.com>
2023-11-02 06:33:24 +0200

    Add Categories Query

---
+++ app/(dashboard)/(routes)/search/_components/catogries.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { Category } from "@prisma/client";
+import { FcDatabase } from "react-icons/fc";
+import { GiArtificialHive } from "react-icons/gi";
+import { BsRobot } from "react-icons/bs";
+import {
+  AiOutlineHtml5,
+  AiOutlineMobile,
+  AiOutlineDatabase,
+} from "react-icons/ai";
+import { FaComputer } from "react-icons/fa6";
+import { FaNetworkWired } from "react-icons/fa";
+import { IconType } from "react-icons/lib";
+import CategoryItem from "./category-item";
+
+interface CategoriesProps {
+  items: Category[];
+}
+
+const iconMap: Record<Category["name"], IconType> = {
+  "Artificial Intelligence": GiArtificialHive,
+  "Data Science": AiOutlineDatabase,
+  "Machine Learning": BsRobot,
+  "Web Development": AiOutlineHtml5,
+  "Mobile Development": AiOutlineMobile,
+  "Computer Science": FaComputer,
+  "Information Technology": FaNetworkWired,
+};
+
+const Categories = ({ items }: CategoriesProps) => {
+  return (
+    <div className="flex items-center gap-x2 overflow-x-auto pb-2">
+      {items.map((item) => (
+        <CategoryItem
+          key={item.id}
+          label={item.name}
+          icon={iconMap[item.name]}
+          value={item.id}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Categories;
--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { db } from "@/lib/db";
+import Categories from "./_components/catogries";
 
-const SearchPage = () => {
-  return <div>SearchPage</div>;
+const SearchPage = async () => {
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
+  return (
+    <div className="p-6">
+      <Categories items={categories} />
+    </div>
+  );
 };
 
 export default SearchPage;
