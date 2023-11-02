7e7edf3a1da4c1ba7649690ea97ecfb25d6069ab
Nabil Hadi <napilfadl@gmail.com>
2023-11-02 13:49:48 +0200

    Setup Course Grid and Card UI

---
+++ actions/get-courses.ts
@@ -0,0 +1,64 @@
+import { Category, Course } from "@prisma/client";
+
+import { getProgress } from "@/actions/get-progress";
+import { db } from "@/lib/db";
+
+export type CourseWithProgressWithCategory = Course & {
+  category: Category | null;
+  chapters: { id: string }[];
+  progress: number | null;
+};
+
+type GetCourses = {
+  userId: string;
+  title?: string;
+  categoryId?: string;
+};
+
+export const getCourses = async ({
+  userId,
+  title,
+  categoryId,
+}: GetCourses): Promise<CourseWithProgressWithCategory[]> => {
+  try {
+    const courses = await db.course.findMany({
+      where: {
+        isPublished: true,
+        title: {
+          contains: title,
+        },
+        categoryId,
+      },
+      include: {
+        category: true,
+        chapters: {
+          where: {
+            isPublished: true,
+          },
+          select: {
+            id: true,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    const coursesWithProgress: CourseWithProgressWithCategory[] =
+      await Promise.all(
+        courses.map(async (course) => {
+          const progress = await getProgress(userId, course.id);
+          return {
+            ...course,
+            progress,
+          };
+        })
+      );
+
+    return coursesWithProgress;
+  } catch (error) {
+    console.log("[GET_COURSES]", error);
+    return [];
+  }
+};
---
+++ components/courses-list.tsx
@@ -0,0 +1,34 @@
+import { CourseWithProgressWithCategory } from "@/actions/get-courses";
+import { Category, Course } from "@prisma/client";
+import CourseCard from "@/components/course-card";
+
+interface CoursesListProps {
+  items: CourseWithProgressWithCategory[];
+}
+
+const CoursesList = ({ items }: CoursesListProps) => {
+  return (
+    <div>
+      <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
+        {items.map((item) => (
+          <CourseCard
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            imageUrl={item.imageUrl!}
+            chaptersLength={item.chapters.length}
+            progress={item.progress}
+            category={item?.category?.name!}
+          />
+        ))}
+      </div>
+      {items.length === 0 && (
+        <div className="text-center text-sm text-muted-foreground mt-10">
+          No Courses Found
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CoursesList;
